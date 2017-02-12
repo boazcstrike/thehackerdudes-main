6f8a9b6eb22bf0146e73a2b51ca945d05c202c1c
boazcstrike <boazcstrike@yahoo.com.sg>
2017-02-12 17:54:12 +0800

    Minor updates

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="EN">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>LBJ</title>
+
+		<link rel="icon" href="img/site-icon.png">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">  
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+
+		<style>
+
+			.hollow-btn{
+				position:relative;
+				text-decoration:none;
+				color:white;
+				background:none;
+				border:solid white 2px;
+				padding: 10px 20px 10px 20px;
+				margin-top:50px;
+				font-size:2.6vh;
+				font-family:'Open Sans';
+				-o-transition:.25s;
+			  	-ms-transition:.25s;
+			  	-moz-transition:.25s;
+			  	-webkit-transition:.25s;
+			}
+
+			.hollow-btn:hover{
+				border:solid #8E0404 2px;
+				background:rgba(255,255,255,0.2);
+			}
+
+		</style>
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body style="min-width: 800px; min-height: 600px; background-image: url('img/site-bg.png');">
+
+		<div class="container" style="margin-top:5em; width:500px;">
+
+			<h1 style="margin-top:10px;" class="text-center">Hello World.</h1>
+			<hr>
+			<h3 class="text-center">Welcome to <span style="color: #44d7a8;">thehackerdudes.com</span></h3>
+			<br>
+
+			<h5 class="text-center"> This site is currently under development.</h5>
+			<h5 class="text-center">Please come back for a while.</h5>
+
+			<br><br>
+
+			<h3 class="text-center">Fellow CODEC,</h3>
+
+			<br>
+
+			<h5 class="text-center">here's the link to your website</h5>
+			<center><a href="http://thehackerdudes.com/CODEC/">thehackerdudes.com/CODEC/</a></center>
+
+			<br>
+
+			<h5 class="text-center">here's the link to CODEC Evaluation Survey</h5>
+			<center><a href="http://thehackerdudes.com/CODEC/">thehackerdudes.com/CODEC/survey.php</a></center>
+
+		</div>
+		
+
+		<!-- jQuery -->
+		<script src="js/jquery-3.1.1.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
